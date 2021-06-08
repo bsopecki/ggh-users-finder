f862ad29e92e4b03d490ca1996a862087e052103
Bartłomiej Sopecki <bartlomiej.sopecki@mobica.com>
2021-06-08 11:45:29 +0200

    refactoring and replace dummy data to http request

--- gh-users-finder/src/components/layout/Navbar.js
+++ gh-users-finder/src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const Navbar = ({ icon, title }) => {
     return (
         <nav className='navbar bg-primary'>
-            <h2><i className={icon}></i>{title}</h2>
+            <h2><i style={{ marginRight: '10px' }} className={icon}></i>{title}</h2>
         </nav>
     )
 }
